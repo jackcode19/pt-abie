ce72050e9d13cdb6ad4ad4e5ea65aff9cd1a0fb3
Jackcode <jackcode190203@gmail.com>
2024-06-29 15:43:44 +0700

    update

--- app/Http/Controllers/Site/ProductController.php
+++ app/Http/Controllers/Site/ProductController.php
@@ -6,15 +6,21 @@ use App\Http\Controllers\Controller;
 use App\Models\Banner;
 use App\Models\Product;
 use App\Models\Service;
+use Exception;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
 
 class ProductController extends Controller
 {
     public function product()
     {
-        $dataProduct = Product::get();
-        $banner = Banner::where('page', 'product')->first();
-        return view('frontend.product.product', compact('dataProduct', 'banner'));
+        try{
+            $dataProduct = Product::get();
+            $banner = Banner::where('page', 'product')->first();
+            return view('frontend.product.product', compact('dataProduct', 'banner'));
+        }catch(Exception $error) { 
+            return $error->getMessage();
+        }
     }
 
     public function productDetail($id)
